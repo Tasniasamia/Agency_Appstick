90680be4c540b426595723c6c643447128394e21
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-09-19 23:40:52 +0600

    header done

--- app/components/site/layout/header.js
+++ app/components/site/layout/header.js
@@ -1,5 +1,4 @@
 "use client";
-
 import { Dropdown } from "antd";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,6 +7,9 @@ import { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { GoDotFill } from "react-icons/go";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { FaBars } from "react-icons/fa";
+import { IoChevronDownOutline } from "react-icons/io5";
 
 const Header = () => {
   const [theme, setTheme] = useState(() => {
@@ -35,13 +37,9 @@ const Header = () => {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
   const [isOpen2, setIsOpen2] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  const handleDropdownVisibility = () => {
-    setIsOpen(!isOpen);
-  };
-  const handleDropdownVisibility2 = () => {
-    setIsOpen2(!isOpen2);
-  };
+
   return (
     <div className="dark:bg-[#0F172A] h-[120px] flex flex-col items-center">
       <div className="container flex justify-between items-center ">
@@ -59,7 +57,7 @@ const Header = () => {
           height={43.67}
           className="h-[43.67px] w-[167.67px] object-fill dark:block hidden"
         />
-        <div className="flex gap-[60px] items-center">
+        <div className="lg:flex hidden gap-[60px] items-center">
           <Dropdown
             trigger="hover"
             overlay={
@@ -333,6 +331,301 @@ const Header = () => {
           </div>
           <button className="bg-primary px-[28px] py-[12px]  text-white cursor-pointer">Log In</button>
         </div>
+        <div className="lg:hidden block">
+          {!open ? (
+            <FaBars
+              onClick={() => setOpen(!open)}
+              className="lg:hidden text-2xl dark:text-white text-black cursor-pointer"
+            />
+          ) : (
+            <IoMdClose
+              onClick={() => setOpen(!open)}
+              className="lg:hidden text-2xl dark:text-white text-black cursor-pointer"
+            />
+          )}
+        </div>
+      </div>
+         {/* For mobile screen */}
+         <div
+        className={`absolute dark:bg-[#0F172A] bg-white bg-opacity-[92%] z-50 top-[120px] left-0 w-[100%] lg:hidden  pt-5 pb-10 flex flex-col px-5 gap-5 ${
+          open ? "flex" : "hidden"
+        } transition-all duration-300`}
+        style={{ filter: "drop-shadow(0px  20px 10px rgba(0, 0, 0, 0.20))" }}
+      >
+       <Dropdown
+            trigger="hover"
+            overlay={
+              <div className="rounded-sm bg-white relative top-3  shadow-sm w-fit">
+                <>
+                  <div
+                    className={`py-2 flex items-center gap-2 group ${
+                      pathname == "/"
+                        ? "bg-primary text-white"
+                        : "text-black bg-white"
+                    } hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200`}
+                  >
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/"}
+                    >
+                      Home 1
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-2"}
+                    >
+                      Home 2
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-3"}
+                    >
+                      Home 3
+                    </Link>
+                  </div>
+                </>
+              </div>
+            }
+          >
+            <div className="flex h-full items-center gap-x-2 cursor-pointer group">
+              <GoDotFill className="text-primary !text-[14px]" />
+
+              <p className="nav-link dark:text-white !text-[18px] text-[#02050A] group-hover:text-primary duration-200 transition-all">
+                Home
+              </p>
+              <FaPlus className="font-bold dark:text-white text-[#02050A] !text-[12px] group-hover:text-primary duration-300 transition-all" />
+            </div>
+          </Dropdown>
+          <Dropdown
+            trigger="hover"
+            overlay={
+              <div className="rounded-sm bg-white relative top-3  shadow-sm w-fit">
+                <>
+                  <div
+                    className={`py-2 flex items-center gap-2 group ${
+                      pathname == "/"
+                        ? "bg-primary text-white"
+                        : "text-black bg-white"
+                    } hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200`}
+                  >
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/"}
+                    >
+                      Home 1
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-2"}
+                    >
+                      Home 2
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-3"}
+                    >
+                      Home 3
+                    </Link>
+                  </div>
+                </>
+              </div>
+            }
+          >
+            <div className="flex h-full items-center gap-x-2 cursor-pointer group">
+              <GoDotFill className="text-primary !text-[14px]" />
+
+              <p className="nav-link dark:text-white !text-[18px] text-[#02050A] group-hover:text-primary duration-200 transition-all">
+                Page
+              </p>
+              <FaPlus className="font-bold dark:text-white text-[#02050A] !text-[12px] group-hover:text-primary duration-300 transition-all" />
+            </div>
+          </Dropdown>
+          <Dropdown
+            trigger="hover"
+            overlay={
+              <div className="rounded-sm bg-white relative top-3  shadow-sm w-fit">
+                <>
+                  <div
+                    className={`py-2 flex items-center gap-2 group ${
+                      pathname == "/"
+                        ? "bg-primary text-white"
+                        : "text-black bg-white"
+                    } hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200`}
+                  >
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/"}
+                    >
+                      Home 1
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-2"}
+                    >
+                      Home 2
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-3"}
+                    >
+                      Home 3
+                    </Link>
+                  </div>
+                </>
+              </div>
+            }
+          >
+            <div className="flex h-full items-center gap-x-2 cursor-pointer group">
+              <GoDotFill className="text-primary !text-[14px]" />
+
+              <p className="nav-link dark:text-white !text-[18px] text-[#02050A] group-hover:text-primary duration-200 transition-all">
+                Service
+              </p>
+              <FaPlus className="font-bold dark:text-white text-[#02050A] !text-[12px] group-hover:text-primary duration-300 transition-all" />
+            </div>
+          </Dropdown>
+          <Dropdown
+            trigger="hover"
+            overlay={
+              <div className="rounded-sm bg-white relative top-3  shadow-sm w-fit">
+                <>
+                  <div
+                    className={`py-2 flex items-center gap-2 group ${
+                      pathname == "/"
+                        ? "bg-primary text-white"
+                        : "text-black bg-white"
+                    } hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200`}
+                  >
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/"}
+                    >
+                      Home 1
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-2"}
+                    >
+                      Home 2
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-3"}
+                    >
+                      Home 3
+                    </Link>
+                  </div>
+                </>
+              </div>
+            }
+          >
+            <div className="flex h-full items-center gap-x-2 cursor-pointer group">
+              <GoDotFill className="text-primary !text-[14px]" />
+
+              <p className="nav-link dark:text-white !text-[18px] text-[#02050A] group-hover:text-primary duration-200 transition-all">
+                Portfolio
+              </p>
+              <FaPlus className="font-bold dark:text-white text-[#02050A] !text-[12px] group-hover:text-primary duration-300 transition-all" />
+            </div>
+          </Dropdown>
+          <Dropdown
+            trigger="hover"
+            overlay={
+              <div className="rounded-sm bg-white relative top-3  shadow-sm w-fit">
+                <>
+                  <div
+                    className={`py-2 flex items-center gap-2 group ${
+                      pathname == "/"
+                        ? "bg-primary text-white"
+                        : "text-black bg-white"
+                    } hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200`}
+                  >
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/"}
+                    >
+                      Home 1
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-2"}
+                    >
+                      Home 2
+                    </Link>
+                  </div>
+                  <div className="py-2 flex items-center gap-2 group hover:bg-primary hover:text-white rounded-sm text-base text-secondary px-4 transition-all duration-200">
+                    {/* {item?.icon} */}
+                    <Link
+                      className="group-hover:text-white transition-all duration-200"
+                      href={"/home-3"}
+                    >
+                      Home 3
+                    </Link>
+                  </div>
+                </>
+              </div>
+            }
+          >
+            <div className="flex h-full items-center gap-x-2 cursor-pointer group">
+              <GoDotFill className="text-primary !text-[14px]" />
+
+              <p className="nav-link dark:text-white !text-[18px] text-[#02050A] group-hover:text-primary duration-200 transition-all">
+                Blog
+              </p>
+              <FaPlus className="font-bold dark:text-white text-[#02050A] !text-[12px] group-hover:text-primary duration-300 transition-all" />
+            </div>
+          </Dropdown>
+          <div className={`nav-link `} onClick={handleToggle}>
+            <div>
+              {theme === "light" ? (
+                <MdDarkMode
+                  className="text-black cursor-pointer"
+                  title="dark mode"
+                />
+              ) : (
+                <MdLightMode
+                  className="text-white cursor-pointer"
+                  title={"light mode"}
+                />
+              )}
+            </div>
+
+          </div>
+        <button className="cursor-pointer bg-primary text-white px-[32px] py-[16px] hover:scale-105 duration-300 rounded-[4px] transition-all">
+          Join Us
+        </button>
       </div>
     </div>
   );
